82a7de58225da5c4010743163f7c5257b25c7e38
Kimi Maulana <kimi93.com@gmail.com>
2025-03-15 11:46:07 +0700

    add image background

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      Home
+    <main className='w-screen h-screen relative'>
+      <div className='flex items-center w-full h-full'>
+        <Image src="/main-bg.webp" alt="Gambar latar belakang" layout="fill" objectFit="cover" />
+      </div>
     </main>      
   );
 }
